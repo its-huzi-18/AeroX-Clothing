622e978388d3e290d7f956a854b596d2522201c1
its-huzi-18 <157364561+its-huzi-18@users.noreply.github.com>
2025-02-28 01:26:08 +0500

    banner-2

--- src/app/components/ThirdSection.tsx
+++ src/app/components/ThirdSection.tsx
@@ -13,13 +13,15 @@ height={390}
 alt='1st'
 />
 <AnimatedCard width='540px' height='320px'>
+  <div className='flex justify-center items-center object-contain w-[460px] h-[200px]'>
 <Image 
 className='shadow-lg rounded-md'
-src={'/Images/Banner-22.jpg'}
-width={590}
-height={340}
+src={'/Images/kk.png'}
+width={470}
+height={220}
 alt='2nd'
 />
+</div>
 </AnimatedCard>
 
     </div>
