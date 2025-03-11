e373be5ef716e95f411ae9795694b36c8a3f3993
its-huzi-18 <157364561+its-huzi-18@users.noreply.github.com>
2025-03-11 16:44:10 +0500

    ImageCarosel

--- src/app/components/ImageCarousel.tsx
+++ src/app/components/ImageCarousel.tsx
@@ -28,7 +28,10 @@ const ImageCarousel = () => {
   ];
 
   return (
-    <div className='relative w-full  '>
+    <div className='relative w-full'>
+      {/* Preload the first image */}
+      <link rel="preload" href={images[0]} as="image" />
+
       <div className='overflow-hidden rounded-lg shadow-xl'>
         <Slider {...settings}>
           {images.map((image, index) => (
@@ -40,6 +43,8 @@ const ImageCarousel = () => {
                 width={1920} // Ensure explicit width
                 height={1080} // Ensure explicit height
                 className='rounded-md h-[250px] md:h-auto'
+                priority={index === 0} // Prioritize the first image
+                loading={index === 0 ? 'eager' : 'lazy'} // Lazy load other images
               />
             </div>
           ))}
